bdec371dee970cc920699fb3c5d43a7c8c7982fa
kred01992 <101710645+kred01992@users.noreply.github.com>
2026-02-07 09:35:26 +0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_INOI_A75_Elegance-user
-add_lunch_combo omni_INOI_A75_Elegance-userdebug
-add_lunch_combo omni_INOI_A75_Elegance-eng
+add_lunch_combo twrp_INOI_A75_Elegance-user
+add_lunch_combo twrp_INOI_A75_Elegance-userdebug
+add_lunch_combo twrp_INOI_A75_Elegance-eng
